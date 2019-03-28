ce79a457b0996dc55f15d8013507b3560e6417a2
Ashley Manzo <40074384+amanzo5@users.noreply.github.com>
2019-03-27 23:25:01 -0700

    Create Operator.cpp

---
+++ TokenCalculator/Operator.cpp
@@ -0,0 +1,12 @@
+/*Operator.cpp*/
+
+#include "Operator.h"
+#include "Number.h"
+
+
+Operator::Operator()
+{
+  //default constructor
+}
+
+//going to be implementing operate in the plus and minus classes
