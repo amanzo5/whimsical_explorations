e65b71bbe93d412d01ec75087724c29c84f7eec4
Ashley Manzo <40074384+amanzo5@users.noreply.github.com>
2019-03-27 23:25:33 -0700

    Create Token.cpp

---
+++ TokenCalculator/Token.cpp
@@ -0,0 +1,14 @@
+/*Token.cpp*/
+
+#include "Token.h"
+
+//have the constructor the toString is abstract
+Token::Token()
+{
+    
+}
+
+//toString function is abstract in token will get implemented in number 
+
+
+
